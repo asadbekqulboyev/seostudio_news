116e4fdba30816abe8d2482f10d33b8fbcff2ee5
asadbek <asadbekqulboyev@gmail.com>
2025-06-08 17:48:37 +0500

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 $(".seo_section .button").click(function (e) {
   e.preventDefault();
-  $(".seo_section .hiden_text").addClass("no-after");
-  $(this).fadeOut(200);
+
+  // Matnni ko‘rsatish/yashirish
+  $(".seo_section .hiden_text").toggleClass("no-after");
+
+  // Tugma matnini almashtirish
+  if ($(".seo_section .hiden_text").hasClass("no-after")) {
+    $(this).text("Свернуть текст");
+  } else {
+    $(this).text("Развернуть текст");
+  }
+});
+
+$(".analytics_page_item .button.no_bg").click(function (e) {
+  e.preventDefault();
+  $(this).prev(".hiden_text").toggleClass("no-after");
+  if ($(this).prev(".hiden_text").hasClass("no-after")) {
+    $(this).text("Свернуть текст");
+  } else {
+    $(this).text("Развернуть текст");
+  }
 });
