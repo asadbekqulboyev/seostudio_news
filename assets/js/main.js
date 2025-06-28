9d09f906818d4fca5a6c1103be996be0995f3d02
asadbek <asadbekqulboyev@gmail.com>
2025-06-28 19:15:54 +0500

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,20 @@ $(".analytics_page_item .button.no_bg").click(function (e) {
 Inputmask('+7 (999) 999-99-99').mask($('input[type="tel"]'))
 $(".analytics_page .comments_item:nth-child(1)").css('height',$('.comments_item:nth-child(2)').height() + $('.comments_item:nth-child(3)').height() + 149 + 'px')
 $(".analytics_page .comments_item:nth-child(3)").css('top',$('.comments_item:nth-child(2)').height() + 80 + 'px')
+$(".no_content a").click(function (e) {
+  e.preventDefault();
+  $(this).parents('.no_content').fadeOut(0);
+  $(this).parents('.all_content').addClass('open')
+  $(this).parents('.all_content').next('.content_btn').addClass('d-flex');
+  
+})
+$('.content_btn').click(function (e) {
+  e.preventDefault();
+  $(this).prev('.all_content').children('.no_content').fadeIn(0);
+  $(this).prev('.all_content').removeClass('open')
+  $(this).removeClass('d-flex');
+  $(this).prev('.tag_columns.last').toggle({'display':'flex'});
+})
+$('.content_btn.items_btn').click(function (e) {
+  $(this).toggleClass('active')
+})
